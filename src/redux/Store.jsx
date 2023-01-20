f9f74508db72ac8e068053b92dc15379757c4b7d
Oleksandr Volik <polskasasha@gmail.com>
2023-01-20 20:27:37 +0200

    change store method

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,49 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 import { persistedReducer } from './contactsSlice';
-import { filterReducer } from './FilterSlice';
+import { filterReducer } from './filterSlice';
 
 export const store = configureStore({
   reducer: {
     contacts: persistedReducer,
     filter: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
-
-
-
-//======================================WAS==================================//
-// import { configureStore } from "@reduxjs/toolkit";
-// import { persistStore, FLUSH, REHYDRATE,PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
-// import { persistedReducer } from "./Actions";
-
-// export const store = configureStore({
-//   reducer: {
-//     contacts: persistedReducer,
-//   },
-//   middleware: getDefaultMiddleware =>
-//   getDefaultMiddleware({
-//     serializableCheck: {
-//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//     },
-//   }),
-//   devTools: process.env.NODE_ENV === 'development',
-// });
-
-// export const persistor = persistStore(store);
