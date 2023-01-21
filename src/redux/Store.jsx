8f613751650a1aa318becefa37253c6661c3c962
Oleksandr Volik <polskasasha@gmail.com>
2023-01-21 04:25:47 +0200

    delete local storage

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -8,3 +8,34 @@ export const store = configureStore({
     filter: filterReducer,
   },
 });
+
+
+//===================================Delete local storage====================================//
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import { persistedReducer } from './contactsSlice';
+// import { filterReducer } from './FilterSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: persistedReducer,
+//     filter: filterReducer,
+//   },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export const persistor = persistStore(store);
